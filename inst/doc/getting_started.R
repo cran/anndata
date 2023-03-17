17e5ce377a0a6850369f8242d3a747f171d70c5e
Robrecht Cannoodt <rcannood@gmail.com>
2023-03-17 13:50:06 +0000

    version 0.7.5.6

--- inst/doc/getting_started.R
+++ inst/doc/getting_started.R
@@ -7,73 +7,73 @@ knitr::opts_chunk$set(
 knitr::opts_chunk$set(eval = reticulate::py_module_available("anndata"))
 
 ## -----------------------------------------------------------------------------
-library(anndata)
-
-ad <- AnnData(
-  X = matrix(1:6, nrow = 2),
-  obs = data.frame(group = c("a", "b"), row.names = c("s1", "s2")),
-  var = data.frame(type = c(1L, 2L, 3L), row.names = c("var1", "var2", "var3")),
-  layers = list(
-    spliced = matrix(4:9, nrow = 2),
-    unspliced = matrix(8:13, nrow = 2)
-  ),
-  obsm = list(
-    ones = matrix(rep(1L, 10), nrow = 2),
-    rand = matrix(rnorm(6), nrow = 2),
-    zeros = matrix(rep(0L, 10), nrow = 2)
-  ),
-  varm = list(
-    ones = matrix(rep(1L, 12), nrow = 3),
-    rand = matrix(rnorm(6), nrow = 3),
-    zeros = matrix(rep(0L, 12), nrow = 3)
-  ),
-  uns = list(
-    a = 1, 
-    b = data.frame(i = 1:3, j = 4:6, value = runif(3)),
-    c = list(c.a = 3, c.b = 4)
-  )
-)
-
-ad
+#  library(anndata)
+#  
+#  ad <- AnnData(
+#    X = matrix(1:6, nrow = 2),
+#    obs = data.frame(group = c("a", "b"), row.names = c("s1", "s2")),
+#    var = data.frame(type = c(1L, 2L, 3L), row.names = c("var1", "var2", "var3")),
+#    layers = list(
+#      spliced = matrix(4:9, nrow = 2),
+#      unspliced = matrix(8:13, nrow = 2)
+#    ),
+#    obsm = list(
+#      ones = matrix(rep(1L, 10), nrow = 2),
+#      rand = matrix(rnorm(6), nrow = 2),
+#      zeros = matrix(rep(0L, 10), nrow = 2)
+#    ),
+#    varm = list(
+#      ones = matrix(rep(1L, 12), nrow = 3),
+#      rand = matrix(rnorm(6), nrow = 3),
+#      zeros = matrix(rep(0L, 12), nrow = 3)
+#    ),
+#    uns = list(
+#      a = 1,
+#      b = data.frame(i = 1:3, j = 4:6, value = runif(3)),
+#      c = list(c.a = 3, c.b = 4)
+#    )
+#  )
+#  
+#  ad
 
 ## -----------------------------------------------------------------------------
-ad$X
-ad$obs
-ad$obsm[["ones"]]
-ad$layers[["spliced"]]
-ad$uns[["b"]]
+#  ad$X
+#  ad$obs
+#  ad$obsm[["ones"]]
+#  ad$layers[["spliced"]]
+#  ad$uns[["b"]]
 
 ## ---- eval = FALSE------------------------------------------------------------
 #  read_h5ad("pbmc_1k_protein_v3_processed.h5ad")
 
 ## -----------------------------------------------------------------------------
-view <- ad[, 2]
-view
-view$is_view
-
-ad[,c("var1", "var2")]
-ad[-1, ]
-
-## -----------------------------------------------------------------------------
-ad$X[,c("var1", "var2")]
-ad$X[-1, , drop = FALSE]
-ad$X[, 2] <- 10
+#  view <- ad[, 2]
+#  view
+#  view$is_view
+#  
+#  ad[,c("var1", "var2")]
+#  ad[-1, ]
 
 ## -----------------------------------------------------------------------------
-ad$layers["unspliced"]
-ad$layers["unspliced"][,c("var2", "var3")]
+#  ad$X[,c("var1", "var2")]
+#  ad$X[-1, , drop = FALSE]
+#  ad$X[, 2] <- 10
 
 ## -----------------------------------------------------------------------------
-ad2 <- ad
-
-ad$X[,2] <- 10
-
-list(ad = ad$X, ad2 = ad2$X)
+#  ad$layers["unspliced"]
+#  ad$layers["unspliced"][,c("var2", "var3")]
 
 ## -----------------------------------------------------------------------------
-ad3 <- ad$copy()
+#  ad2 <- ad
+#  
+#  ad$X[,2] <- 10
+#  
+#  list(ad = ad$X, ad2 = ad2$X)
 
-ad$X[,2] <- c(3, 4)
-
-list(ad = ad$X, ad3 = ad3$X)
+## -----------------------------------------------------------------------------
+#  ad3 <- ad$copy()
+#  
+#  ad$X[,2] <- c(3, 4)
+#  
+#  list(ad = ad$X, ad3 = ad3$X)
 
--- inst/doc/scanpy_demo.R
+++ inst/doc/scanpy_demo.R
@@ -7,26 +7,26 @@ knitr::opts_chunk$set(
 knitr::opts_chunk$set(eval = reticulate::py_module_available("anndata") && reticulate::py_module_available("scanpy"))
 
 ## -----------------------------------------------------------------------------
-library(anndata)
-library(reticulate)
-sc <- import("scanpy")
-
-url <- "https://cf.10xgenomics.com/samples/cell-exp/6.0.0/SC3_v3_NextGem_DI_CellPlex_CSP_DTC_Sorted_30K_Squamous_Cell_Carcinoma/SC3_v3_NextGem_DI_CellPlex_CSP_DTC_Sorted_30K_Squamous_Cell_Carcinoma_count_sample_feature_bc_matrix.h5"
-ad <- sc$read_10x_h5("dataset.h5", backup_url = url)
-
-ad
-
-## -----------------------------------------------------------------------------
-ad[1:5, 3:5]
-dim(ad)
+#  library(anndata)
+#  library(reticulate)
+#  sc <- import("scanpy")
+#  
+#  url <- "https://cf.10xgenomics.com/samples/cell-exp/6.0.0/SC3_v3_NextGem_DI_CellPlex_CSP_DTC_Sorted_30K_Squamous_Cell_Carcinoma/SC3_v3_NextGem_DI_CellPlex_CSP_DTC_Sorted_30K_Squamous_Cell_Carcinoma_count_sample_feature_bc_matrix.h5"
+#  ad <- sc$read_10x_h5("dataset.h5", backup_url = url)
+#  
+#  ad
 
 ## -----------------------------------------------------------------------------
-sc$pp$filter_cells(ad, min_genes = 200)
-sc$pp$filter_genes(ad, min_cells = 3)
-sc$pp$normalize_per_cell(ad)
-sc$pp$log1p(ad)
+#  ad[1:5, 3:5]
+#  dim(ad)
 
 ## -----------------------------------------------------------------------------
-library(Matrix)
-rowMeans(ad$X[1:10, ])
+#  sc$pp$filter_cells(ad, min_genes = 200)
+#  sc$pp$filter_genes(ad, min_cells = 3)
+#  sc$pp$normalize_per_cell(ad)
+#  sc$pp$log1p(ad)
+
+## -----------------------------------------------------------------------------
+#  library(Matrix)
+#  rowMeans(ad$X[1:10, ])
 
